70cb68a45c7056e6e54dd5ead0e30cc336db8249
jay sharma <jay@cloudganga.com>
2018-03-11 15:50:23 +0530

    mongodb port changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.set('debug',true);
-mongoose.connect('mongodb://13.127.56.24:27017/test');
+mongoose.connect('mongodb://52.66.180.44:27017/test');
 module.exports = {mongoose};
